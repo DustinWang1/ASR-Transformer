270c4948cb98a7c66c79ca545a54a142a53e638f
DustinWang1 <wangd385@gmail.com>
2024-02-29 13:20:23 -0800

    Version uses pytorch transformer. Much easier now.

---
+++ Model.py
@@ -0,0 +1,100 @@
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class CNNLayerNorm(nn.Module):
+    def __init__(self, n_feats):
+        super(CNNLayerNorm, self).__init__()
+        self.layer_norm = nn.LayerNorm(n_feats)
+
+    def forward(self, x):
+        # (batch, channel, feature, time)
+        x = x.transpose(2, 3).contiguous()  # (batch, channel, time, feature)
+        x = self.layer_norm(x)
+        return x.transpose(2, 3).contiguous()  # (batch, channel feature, time)
+
+
+# ResCNN cell
+class ResCNN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats):
+        super(ResCNN, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel // 2)
+        self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel // 2)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.layer_norm1 = CNNLayerNorm(n_feats)
+        self.layer_norm2 = CNNLayerNorm(n_feats)
+
+    def forward(self, x):
+        residual = x
+        x = self.layer_norm1(x)
+        x = nn.functional.gelu(x)
+        x = self.dropout1(x)
+        x = self.cnn1(x)
+        x = self.layer_norm2(x)
+        x = nn.functional.gelu(x)
+        x = self.dropout2(x)
+        x = self.cnn2(x)
+        x += residual
+        return x
+
+class SpeechRecognitionModel(nn.Module):
+    def __init__(self, vocab_size: int = 29, n_cnn_layers: int = 4, d_model: int = 512, n_feats: int = 128, stride: int = 1, dropout: float = 0.1):
+        super().__init__()
+
+        #vocab size is num characters
+        self.label_embedding = InputEmbeddings(d_model=512, vocab_size=29)
+        self.cnn = nn.Conv2d(1, 4, 3, stride=stride,
+                             padding=2 // 2)
+        self.resCNNLayers = nn.Sequential(*[
+            ResCNN(4, 4, kernel=3, stride=1, dropout=dropout, n_feats=n_feats)
+            for _ in range(n_cnn_layers)
+        ])
+
+        self.transformer = nn.Transformer(d_model=512, nhead=8, batch_first=True)
+
+        self.projection = nn.Linear(d_model, vocab_size)
+
+    def forward(self, spec, label):
+        x = self.cnn(spec)
+        x = self.resCNNLayers(x)  # (batch, channel, n_feats, seq_len)
+
+        # Prepare for transformer
+        # (batch, seq_len, d_model) d_model = 512
+        sizes = x.size()
+        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
+        x = x.transpose(1, 2)  # (batch, seq, d_model)
+
+        # Input Embedding for label
+        label = self.label_embedding(label)
+
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(label.size(1))
+        x = self.transformer(src=x, tgt=label, tgt_mask=causal_mask, tgt_is_causal=True)
+
+        # Output of transformer is (batch, seq, embedding)
+        return self.projection(x)  # (batch, seq, vocab_size)
+
+
+
+def build_transformer():
+
+    return SpeechRecognitionModel()
+
+
+
+
+# What special characters for tokenization?
+# Add extra ResCNN?
+# CTCLoss?
---
+++ main.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import os
+import sys
+import torchaudio
+from comet_ml import Experiment
+
+import TrainTest
+import Model
+import utils
+
+def main(learning_rate=5e-4,
+         batch_size=20,
+         epochs=10,
+         train_url="train-clean-100",
+         test_url="test-clean",
+         experiment=Experiment(
+             api_key="RDMwDBjNyTYsfyWcwNEzZQulc",
+             project_name="general",
+             workspace="dustin-wang"
+         )
+         ):
+
+    hparams = {
+        "n_cnn_layers": 4,
+        "n_class": 29,
+        "n_feats": 128,
+        "d_model": 512,
+        "stride": 1,
+        "dropout": 0.1,
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "epochs": epochs
+    }
+
+    log_file = open('/logs', 'w')
+
+    # Redirect sys.stdout to the log file
+    sys.stdout = log_file
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if not os.path.isdir("./data"):
+        os.makedirs("./data")
+
+    train_dataset = torchaudio.datasets.LIBRISPEECH("./data", url=train_url, download=True)
+    test_dataset = torchaudio.datasets.LIBRISPEECH("./data", url=test_url, download=True)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=hparams['batch_size'],
+                                               shuffle=True,
+                                               collate_fn=lambda x: utils.data_processing(x, 'train'),
+                                               **kwargs) # For each batch of samples run this function on them
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=hparams['batch_size'],
+                                              shuffle=False,
+                                              collate_fn=lambda x: utils.data_processing(x, 'valid'),
+                                              **kwargs)
+    # Data Loader -> (spectrograms, labels, input_lengths, output_lengths)
+    # spectrograms -> (batch, channel, n_mels, seq_len)
+    # labels -> (batch, seq_len)
+
+    # with torch.no_grad():
+    #     specs, labels, input_lengths, output_lengths = next(iter(train_loader))
+    #     model = Model.build_transformer()
+    #     output = model(specs, labels)
+    #     print(output)
+
+    model = Model.build_transformer()
+    optimizer = torch.optim.AdamW(model.parameters(), hparams['learning_rate'])
+    criterion = nn.CTCLoss(blank=28).to(device)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=hparams['learning_rate'],
+                                                    steps_per_epoch=int(len(train_loader)),
+                                                    epochs=hparams['epochs'],
+                                                    anneal_strategy='linear')
+
+    iter_meter = utils.IterMeter()
+    for epoch in range(1, epochs + 1):
+        TrainTest.train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
+        TrainTest.test(model, device, test_loader, criterion, epoch, iter_meter, experiment)
+        experiment.end()
+    torch.save(model.state_dict(), "./model_saves")
+
+
+if __name__ == '__main__':
+    main()
